6a342ed2ef4e133351ab94e95ff78f9b143cd984
Narendra Sapare <narendra.sapare@gmail.com>
2026-07-15 13:39:16 +0530

    chore: establish backend development environment

---
+++ backend/src/main/java/com/cartcraft/CartCraftBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cartcraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartCraftBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartCraftBackendApplication.class, args);
+	}
+
+}
